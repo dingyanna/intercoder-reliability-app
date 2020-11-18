b0a06b7d5c65086e4d079b1c4526c9f47c4ff5b0
Yanna Ding <yannading10@gmail.com>
2020-11-18 10:38:37 -0500

    removed unnecessary imports

--- app/components/Calculation.tsx
+++ app/components/Calculation.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 import {
-  InputLabel,
   FormControl,
-  Select,
   Grid,
   Button,
   Typography,
   FormControlLabel,
-  RadioGroup,
-  Radio,
   ThemeProvider,
   Divider,
 } from '@material-ui/core';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import Tooltip from '@material-ui/core/Tooltip';
 import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress, { CircularProgressProps } from '@material-ui/core/CircularProgress';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from './Home.css';
 import { PythonShell } from 'python-shell';
-import Swal from 'sweetalert2';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import IconButton from '@material-ui/core/IconButton';
 import Collapse from '@material-ui/core/Collapse';
@@ -175,7 +169,7 @@ export default function Calculation(): JSX.Element {
   };
 
 
-  const onReset = (e: any): void => {
+  const onReset = (): void => {
     setData('no file chosen');
     setCategories('no file chosen');
     setWeights('no file chosen');
